7e469fd193a4e156674c43430d494c72bc57ff26
MeltMyGpu <kieranburnett98@gmail.com>
2024-07-07 17:38:44 +0100

    Started on the nmea sentence data objects, as having those done first will make data handling much easier

---
+++ src/custom_exceptions.py
@@ -0,0 +1,6 @@
+
+
+
+class InvalidNmeaTypeException(Exception):
+    "Raised when the incorrect type of NMEA sentence was passed to a class initializer "
+    pass
--- src/serial_reader.py
+++ src/serial_reader.py
@@ -1,20 +1,25 @@
 
+
 import serial.tools.list_ports as port_list
 import serial
 import logging
 import inspect
+import os
 
- 
+
 # GLOBAL CONST
+FILE_PATH = os.path.dirname(__file__)
+# os.environ["PYTHONPYCACHEPREFIX"] = FILE_PATH + "/../Cache"
+
 LOG = logging.getLogger(":serial_reader:")
 ERR_STR = "{caller}:: {Error}"
 
 
 # STREAM = serial.Serial('COM4')
 def start_up():
-    open("myapp.log", "w").close() # Clear log file 
-    logging.basicConfig(filename="myapp.log",level=logging.INFO)
-    LOG.info(ERR_STR.format(caller=inspect.currentframe().f_code.co_name, Error= "Startup completed"))
+    open(FILE_PATH + "/../myapp.log", "w").close() # Clear log file 
+    logging.basicConfig(filename= FILE_PATH + "/../myapp.log",level=logging.DEBUG)
+    LOG.info(ERR_STR.format(caller= inspect.currentframe().f_code.co_name, Error= "Startup completed"))
  
 
 def get_com_ports() -> list | None:
@@ -43,7 +48,7 @@ def display_com_ports():
     ports = get_com_ports()
         
     if ports == None:
-        LOG.warning(ERR_STR.format(caller=inspect.currentframe().f_code.co_name, Error= "No active COM ports"))
+        LOG.warning(ERR_STR.format(caller= inspect.currentframe().f_code.co_name, Error= "No active COM ports"))
         raise Exception(" No valid COM ports could be located, is the device plugged in?")
     
     i = 1 
@@ -83,7 +88,7 @@ def display_com_stream(stream: serial = None):
         Exception: If there is not an allocated serial port
     """
     if stream == None:
-        LOG.warning(ERR_STR.format(caller =inspect.currentframe().f_code.co_name, Error="Trying to read COM stream without providing active serial object"))
+        LOG.warning(ERR_STR.format(caller= inspect.currentframe().f_code.co_name, Error= "Trying to read COM stream without providing active serial object"))
         return 
     while True:
         print(_stream_readline(stream) + "\n")
@@ -92,20 +97,21 @@ def display_com_stream(stream: serial = None):
 
 def write_stream_to_file(stream: serial = None):
     if stream == None:
-        LOG.warning(ERR_STR.format(caller =inspect.currentframe().f_code.co_name, Error="Trying to read COM stream without providing active serial object"))
+        LOG.warning(ERR_STR.format(caller= inspect.currentframe().f_code.co_name, Error= "Trying to read COM stream without providing active serial object"))
         return
-    with open("streamData.txt","w") as file: 
+    with open(FILE_PATH + "/../streamData.txt","w") as file: 
         i=0
         while i < 20:
             file.write(_stream_readline(stream) + "\n")
             i += 1
 
+        
             
         
 start_up()
     
 display_com_ports()
-display_com_stream()
+# display_com_stream()
 stream = serial.Serial('COM4')
 write_stream_to_file(stream)
 # display_com_stream(stream)
@@ -113,10 +119,23 @@ write_stream_to_file(stream)
 
 '''
 TODO:
-    - Create a form of serialization for sentences
     - Sort through the types of NMEA sentences
+    - Create a form of serialization for sentences
     - Learn how to interpret the sentences in order to obtain information from them
     - Refactor code base
-
-
 '''
+
+
+def GpsFixData_init_test():
+    from nmea_objects.GpsFixData import GpsFixData
+    test_list = ["$GSGPA", " BLANK"]
+    try:
+        GpsFixData(test_list)
+        LOG.debug(ERR_STR.format(caller= inspect.currentframe().f_code.co_name, Error= "failed"))
+    except ce.InvalidNmeaTypeException as e:
+        LOG.debug(ERR_STR.format(caller= inspect.currentframe().f_code.co_name, Error= "Passed: giving" ))
+        LOG.exception(e)
+        
+
+''' TEST BLOCK'''
+# GpsFixData_init_test()
